ff81e133302116d4caf39955403351b55937b8e7
PabloLerma <plermam@gmail.com>
2016-09-12 23:45:46 +0200

    Add label and button to handle the led

--- ArduinoLampControl/ArduinoLampControl/ViewController.swift
+++ ArduinoLampControl/ArduinoLampControl/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var ledLabel: UILabel!
+    private let session = NSURLSession.sharedSession()
+
+    var ledState: Bool = false {
+        didSet {
+            ledLabel.text = "Led is \(ledState ? "ON" : "OFF")"
+            let url = ledState ? "http://192.168.1.177/$1" : "http://192.168.1.177/$2"
+            session.dataTaskWithURL(NSURL(string: url)!) { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+                print(error)
+            }.resume()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        ledState = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func switchChanged(sender: UISwitch) {
+        ledState = sender.on
     }
-
-
 }
-
